ea8a0ed6e84d8a5b22e601959822c29e9fcb1567
LovelyWolfie <147515562+LovelyWolfie@users.noreply.github.com>
2023-10-17 09:36:44 -0400

    Week3b.js

---
+++ JS Week3b.js
@@ -0,0 +1,20 @@
+console.log('I am inside Week3 B JaveStcript file.');
+
+var ammo = 100;
+console.log(ammo);
+console.log("ammo = " + ammo);
+
+ammo = prompt("type in the number of ammo: ");
+console.log("the new entered ammo = " + ammo);
+
+var a=0;
+var b=0;
+var sum=0;
+
+a = prompt("enter the value: ");
+a = Number(a);
+b = prompt("Tpye a second value: ");
+b = Number(b);
+sum = a + b;
+console.log("The sum of a & b is = " + sum);
+
