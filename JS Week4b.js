035b085ebf21bdbb48617737cdc5273389a6810a
LovelyWolfie <147515562+LovelyWolfie@users.noreply.github.com>
2023-10-24 10:00:52 -0400

    Weekb.js

---
+++ JS Week4b.js
@@ -0,0 +1,16 @@
+let age = 18;
+age = age + 1;
+console.log("You are now " + age + " year old.");
+
+age++; // short-hand notation
+console.log("You are now " + age + "year old.");
+
+let  ammo = 6;
+ammo = ammo - 1;
+ammo--; 
+console.log("Ammo left in chamber: " + ammo);
+
+let balance = 0;
+balance = balance + 4.18;
+balance = balance - 0.50;
+console.log("You pay: " + balance);
