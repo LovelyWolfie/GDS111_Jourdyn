da86068bdd36767fa17eafe413c58284fa7667fc
LovelyWolfie <147515562+LovelyWolfie@users.noreply.github.com>
2023-10-26 11:00:28 -0400

    Week4d.js

---
+++ JS Week4d.js
@@ -0,0 +1,19 @@
+let fName= "Luna"; //variables are case-sensitive 
+let age = 22;
+let salary = 40000.00;
+let employed = true;
+
+console.log(fName + " is " + age + " years old.");
+console.log(" She is making " + salary + " annually. ");
+console.log(typeof fName);
+console.log(typeof age);
+console.log(typeof salary);
+console.log(typeof employed);
+
+fName = 77;
+console.log("fName = " + fName);
+console.log(typeof fName);
+
+const player = ["Luigi","Peach","Pikachu","Ash"];
+console.log(player[0]);
+
